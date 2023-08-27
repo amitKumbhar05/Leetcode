e6ba5dba64064df545c6df37b2f13fe1442ef4d1
Amitkumbhar5 <amitkumbhar2023@gmail.com>
2023-08-27 19:34:21 +0530

    27-08-2023 f1 sql no

--- 550. Game Play Analysis IV.sql
+++ 550. Game Play Analysis IV.sql
@@ -1,4 +1,10 @@
-SELECT ROUND(COUNT(t2.player_id)/COUNT(t1.player_id),2) AS fraction
+# Write your MySQL query statement below
+SELECT
+  ROUND(COUNT(DISTINCT player_id) / (SELECT COUNT(DISTINCT player_id) FROM Activity), 2) AS fraction
 FROM
-(SELECT player_id, MIN(event_date) AS first_login FROM Activity GROUP BY player_id) t1 LEFT JOIN Activity t2
-ON t1.player_id = t2.player_id AND t1.first_login = t2.event_date - 1
+  Activity
+WHERE
+  (player_id, DATE_SUB(event_date, INTERVAL 1 DAY))
+  IN (
+    SELECT player_id, MIN(event_date) AS first_login FROM Activity GROUP BY player_id
+  )
